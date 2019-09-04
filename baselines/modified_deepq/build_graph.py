106aa9eb049c28f5040d989734023ee02a09a9f3
guikarist <guikarist@gmail.com>
2019-09-04 16:16:29 +0800

    Fix wrong place for `has_obs_tmi_mask_ph`

--- baselines/modified_deepq/build_graph.py
+++ baselines/modified_deepq/build_graph.py
@@ -295,25 +295,25 @@ def build_train(make_obs_ph, q_func, num_actions, optimizer, grad_norm_clipping=
         weighted_error = tf.reduce_mean(importance_weights_ph * errors)
         f_error_1 = tf.reduce_sum(tf.square(f_features_t - f_features_tp1), 1)
         f_error_2 = tf.reduce_sum(tf.square(f_features_tmi - f_features_tp1), 1)
-        representation_loss = tf.reduce_mean(tf.maximum(0., margin + f_error_1 - f_error_2))
+        representation_loss = tf.reduce_mean(has_obs_tmi_mask_ph * tf.maximum(0., margin + f_error_1 - f_error_2))
 
         if modified_part is not None:
             if modified_part == 'before':
                 loss = tf.cond(
                     tf.less(update_eps_ph - tf.Variable(exploration_final_eps), tf.Variable(0.0001)),
                     lambda: weighted_error,
-                    lambda: weighted_error + has_obs_tmi_mask_ph * lambda_ * representation_loss
+                    lambda: weighted_error + lambda_ * representation_loss
                 )
             elif modified_part == 'after':
                 loss = tf.cond(
                     tf.less(update_eps_ph - tf.Variable(exploration_final_eps), tf.Variable(0.0001)),
-                    lambda: weighted_error + has_obs_tmi_mask_ph * lambda_ * representation_loss,
+                    lambda: weighted_error + lambda_ * representation_loss,
                     lambda: weighted_error
                 )
             else:
                 raise ValueError("Invalid value of 'modified_part'")
         else:
-            loss = weighted_error + has_obs_tmi_mask_ph * lambda_ * representation_loss
+            loss = weighted_error + lambda_ * representation_loss
 
         # compute optimization op (potentially with gradient clipping)
         if grad_norm_clipping is not None:
